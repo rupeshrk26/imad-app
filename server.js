84c9f30c94d0935d1ed42ccadf294ff29aedbf4a
rupeshrk26 <30711643+rupeshrk26@users.noreply.github.com>
2017-08-12 07:28:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+    res.send("Article one requsted and will be searve");
+});
+
+app.get('/article-two',function(req,res){
+    res.send("Article two requsted and will be searve");
+});
+
+app.get('/article-three',function(req,res){
+    res.send("Article three requsted and will be searve");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
