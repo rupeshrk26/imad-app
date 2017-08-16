b211093d090a33594a7abd8e933134949626b7ed
rupeshrk26 <30711643+rupeshrk26@users.noreply.github.com>
2017-08-16 09:59:09 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var article = {
+var articleOne = {
     title: 'Article One | Rupesh Rathod',
     date:'Aug 12 2017',
+    heading:'Article one',
     content: `<p>
                     Hi, i'm Rupesh and this is my first webapp.this is my first article.Hi, i'm Rupesh and this is my first webapp.this is my first article.Hi, i'm Rupesh and this is my first webapp.this is my first article.Hi, i'm Rupesh and this is my first webapp.this is my first article. 
                 </P>
@@ -21,11 +22,43 @@ var article = {
                     first article.Hi, i'm Rupesh and this is my first webapp.this is my first article.Hi, i'm Rupesh and this is my first weba.this is my first article.
                 </P>`
 };
+function createArticleTemplate(data){
+    var title =data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate=
+        `<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>`;
+    
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createArticleTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
